73bbccb85b0245fd9a0ae1996a0084f4531bdcfc
Daniel Winkler <daniel.winkler@s.wu.ac.at>
2025-05-14 21:50:48 +0200

    added packageDef without R

--- flake.nix
+++ flake.nix
@@ -306,9 +306,7 @@
           sharedLibraries = {
             general = with pkgs; [
               # <- this would be included if any of the subcategories of general are
-              # libgit2
-              gcc
-              gnumake
+              libgit2
             ];
           };
 
@@ -425,6 +423,67 @@
             categories = {
               notes = true;
               rdev = true;
+              markdown = true;
+              general = true;
+              lint = true;
+              format = true;
+              neonixdev = true;
+              test = {
+                subtest1 = true;
+              };
+
+              # enabling this category will enable the go category,
+              # and ALSO debug.go and debug.default due to our extraCats in categoryDefinitions.
+              # go = true; # <- disabled but you could enable it with override or module on install
+
+              # this does not have an associated category of plugins,
+              # but lua can still check for it
+              lspDebugMode = false;
+              # you could also pass something else:
+              # see :help nixCats
+              themer = true;
+              colorscheme = "onedark";
+            };
+            extra = {
+              # to keep the categories table from being filled with non category things that you want to pass
+              # there is also an extra table you can use to pass extra stuff.
+              # but you can pass all the same stuff in any of these sets and access it in lua
+              nixdExtras = {
+                nixpkgs = ''import ${pkgs.path} {}'';
+                # or inherit nixpkgs;
+              };
+            };
+          };
+        nC=
+          { pkgs, name, ... }@misc:
+          {
+            # these also recieve our pkgs variable
+            # see :help nixCats.flake.outputs.packageDefinitions
+            settings = {
+              suffix-path = true;
+              suffix-LD = true;
+              # The name of the package, and the default launch name,
+              # and the name of the .desktop file, is `nixCats`,
+              # or, whatever you named the package definition in the packageDefinitions set.
+              # WARNING: MAKE SURE THESE DONT CONFLICT WITH OTHER INSTALLED PACKAGES ON YOUR PATH
+              # That would result in a failed build, as nixos and home manager modules validate for collisions on your path
+              aliases = [
+                "vim"
+                "vimcat"
+              ];
+
+              # explained below in the `regularCats` package's definition
+              # OR see :help nixCats.flake.outputs.settings for all of the settings available
+              wrapRc = true;
+              configDirName = "nixCats-nvim";
+              # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+              hosts.python3.enable = true;
+              hosts.node.enable = true;
+            };
+            # enable the categories you want from categoryDefinitions
+            categories = {
+              notes = true;
+              rdev = false;
               markdown = true;
               general = true;
               lint = true;
