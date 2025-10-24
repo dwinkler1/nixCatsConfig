9af1a6653a811d90f2500dcef4bef3c33e3128ca
Daniel Winkler <dw@dwinkler.org>
2025-10-24 13:39:00 +1100

    python formatting with ruff

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -4,3 +4,11 @@ vim.g.slime_python_ipython = 1
 local ts_lib = Config.treesitter_helpers
 local global_nodes_python = { 'module' }
 ts_lib.setup_keybindings(global_nodes_python)
+
+require("conform").setup({
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 100,
+    lsp_format = "prefer",
+  },
+})
